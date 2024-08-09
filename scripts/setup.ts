8ca4b96cf9a5cf98dc6eb2c90714bdbdbf7a06d2
hana <motivationisdead@proton.me>
2024-08-09 13:51:08 +0000

    Add setup script

---
+++ scripts/setup.ts
@@ -0,0 +1,22 @@
+import { $ } from 'bun'
+import process from 'node:process'
+
+const repo = process.argv[2]
+const repoUrl = `https://github.com/motivationisdead/${repo}.git`
+
+if (!repo) {
+  console.log('usage: bun setup <repo>')
+  process.exit(1)
+}
+
+try {
+  await $`git clone ${repoUrl} packages/${repo}`
+} catch {
+  process.exit(1)
+}
+
+process.chdir(`packages/${repo}`)
+
+await $`bun i`
+await $`cp ../../.gitignore .`
+await $`echo /.gitignore >> .gitignore`
